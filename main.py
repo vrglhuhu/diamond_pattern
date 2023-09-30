f0ae744b007c8cf2f3d65427600f57375312c624
vrglhuhu <cheanbvergel@gmail.com>
2023-09-30 20:26:52 +0800

    Initial commit.

---
+++ main.py
@@ -0,0 +1,10 @@
+# VERGEL, CHEAN BERNARD V.
+# LAB 1
+# DIAMOND PATTERN
+
+# import the class
+# define the user_interface
+# Ask the user for an odd number
+# create an try an except value error
+
+        
