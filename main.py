d83eac4a5bcc74eacedcb8f32f4c91ec468b2228
vrglhuhu <cheanbvergel@gmail.com>
2023-09-30 20:50:35 +0800

    Supplying the code for every part of psuedocodes.

--- main.py
+++ main.py
@@ -3,8 +3,22 @@
 # DIAMOND PATTERN
 
 # import the class
-# define the user_interface
-# Ask the user for an odd number
-# create an try an except value error
+from diamond_pattern import DiamondPattern
 
-        
+# Define the user_interface function
+def user_interface():
+    # Ask the user for an odd number
+    user_input = input("Enter an odd integer: ")
+
+    # Create a try and except block for handling exceptions
+    try:
+        n = int(user_input)
+        # Create an instance of the DiamondPattern class
+        diamond = DiamondPattern()
+        diamond.print_diamond(n)
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+# Call the user_interface function to start the program
+if __name__ == "__main__":
+    user_interface()
